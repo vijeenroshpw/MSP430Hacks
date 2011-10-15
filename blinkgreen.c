c2ecf695947f8fddba64ac27d87dd9184367959d
Vijeenrosh P.W <hsorhteeniv@gmail.com>
2011-10-15 13:08:32 -0400

    A programe to blink Green LED

--- blinkgreen.c
+++ blinkgreen.c
@@ -8,19 +8,21 @@ main()
 {
   P1DIR=0x40;
   P1OUT=0x40; 
- while(1)
+  while(1)
   {
     delay();
     P1OUT^=0x40;
     delay();
     P1OUT^=0x40;
   }
+
 }
 
 void delay(void)
 {
   unsigned i;
   for(i=0;i<32000;i++);
+
 }
 
     
