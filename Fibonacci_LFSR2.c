8a159c3df36cb74e50abb5c210f0f487338f9896
Vijeenrosh P.W <hsorhteeniv@gmail.com>
2011-12-14 00:12:29 -0500

    Random BOTH LED lighting Using LFSR Method

---
+++ Fibonacci_LFSR2.c
@@ -0,0 +1,16 @@
+#include<msp430.h>
+int main() 
+{
+	TACTL=0x0220;	
+	P1DIR = 0x41;
+	int i;	
+	unsigned lfsr = 0xef1f,bit =0;
+	while(1) {
+		i=0;		
+		bit  = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5) ) & 1;
+  		lfsr =  (lfsr >> 1) | (bit << 15);
+		while(i++ <= 30000);
+		P1OUT = lfsr;
+	}
+}
+
