d84907a3278bd28f1d113510ed820c9d9735aff3
Vijeenrosh P.W <hsorhteeniv@gmail.com>
2012-01-17 13:57:25 -0500

    blinks  GREEN  led

---
+++ Assembly_Codes/blink_green.s
@@ -0,0 +1,16 @@
+org 0xf800
+
+start:mov.w #0x5a80 , 0x0120
+      mov.b #0x40 , &0x22
+      mov.b #0x40 , &0x21
+L1:xor.b #0x40 ,&0x21
+
+    mov.w #0xffff , R7  ;initialize the counter  
+L2: sub.w #1 , R7    ; decrement the counter by one 
+    jnz L2              ; jump to L2 if counter falls to zero
+    jmp L1              ; else unconditionaly jump to L1
+loop:jmp loop           ; indicates programme termination 
+
+org 0xfffe
+dw start
+
